8a89426d2d9b57e0dd3ed83d1de1872db28e7729
FAJCHANG <zoozle27503@gmail.com>
2023-02-18 16:07:38 +0800

    add comment

--- Singleflight/Bathy/main.m
+++ Singleflight/Bathy/main.m
@@ -18,7 +18,7 @@ mat_savePath = [rootPath char(foldName(3))];%保存mat文件
 gaussian_kernel = 50;
 intrinsics_name = 'P4camera_1080.mat';
 fs = 2;
-%% 第一步下采样
+%% 第一步下采样 Step 1 Sampling
 disp('----------step1 DwonSample start----------');
 ignore = 0;
 if ignore
@@ -29,7 +29,7 @@ if ignore
 end
 disp('----------step1 end----------')
 
-%% 第二步绑定视频路径 参数项
+%% 第二步绑定视频路径 参数项  Step 2 Bind video path parameter items
 disp('----------step2 Bundle Video Path Start----------');
 addpath('RANSAC');
 addpath('tracks');
@@ -67,8 +67,10 @@ else
     load([savepath 'tracks_' int2str(TrackWindowSize) '_' int2str(TracksPerFrame) '.mat']);
 end
 disp('Finished Clustering, ');
-%%
-threshold = 0.06; %根据实际调整的参数
+%% background identification
+% 背景识别，如果识别效果很差，可以使用颜色阈值法或者模板匹配，但本文并未提供
+% if the recognition effect is poor, color threshold method or template matching can be used, but this code does not provide
+threshold = 0.02; %根据实际调整的参数
 if ~exist([savepath 'pathAone' int2str(1) '.mat'], 'file')
     [graphA, goodAone] = OneGraph(trackA, threshold);
     backListAone = refineTrack(trackA, goodAone);
@@ -81,18 +83,12 @@ else
 end
 disp('finish one background identification')
 %% 第三步记录控制点，计算外参
-
+% Step 3 Record control points and calculate extrinsics parameters
 disp('----------step3 start-----------');
 disp('开始标定');
 step3.savePath = mat_savePath;
 
 step3.world.gcp_llh = [
-%     22.59574838888889 114.87603830555555 1.211;
-%     22.59587286111111 114.87596880555556 1.529;
-%     22.595866055555557 114.87582841666666 1.226;
-%     22.595963777777776 114.87575544444445 1.47;
-%     22.59599638888889 114.87562186111111 1.306;
-%     22.59612275 114.87541755555554 1.375
     22.59597440 114.87604752 2.08;
     22.59599842 114.87592532 2.17;
     22.59608425 114.87593422 1.96;
@@ -100,20 +96,20 @@ step3.world.gcp_llh = [
     22.59619867 114.87567679 1.98;
     22.59619766 114.87553950 2.16
 ];
-step3.world.o_llh = [22.59597440 114.87604752 2.08];
-% step3.world.o_llh = [22.595853583333334 114.87607558333333 1.921];
-step3.world.uav_llh = [22.59648368 114.87615432 77.30];
-step3.intrinsic = ['./neededData/',intrinsics_name];
+step3.world.o_llh = [22.59597440 114.87604752 2.08];% World origin
+step3.world.uav_llh = [22.59648368 114.87615432 77.30];% UAV initial position
+step3.intrinsic = ['./neededData/',intrinsics_name]; 
 step3.imagePath = ds_image_savePath;
 
 uavInfo_world = gcpllh2NED(step3.world.o_llh,step3.world.uav_llh);
 
-step3.extrinsicsInitialGuess = [uavInfo_world' deg2rad(-36.0) deg2rad(0) deg2rad(-147.6)];   
+step3.extrinsicsInitialGuess = [uavInfo_world' deg2rad(-36.0) deg2rad(0) deg2rad(-147.6)]; %It needs to be adjusted according to the camera position and Euler angles(pitch roll yaw)   
 step3.extrinsicsKnownsFlag = [0,0,0,0,0,0]; 
 
 [gcpUV, Expara] = chooseGCP(step3);
 disp('----------step3 end----------');
-%% 第四步，通过绑定视频路径来验证是否正确,但是不知道会不会发散
+%% 第四步，计算每一帧外参
+% Calculate each frame extrinsics parameters
 step4.savepath = rootPath;
 step4.inputDir = ds_image_savePath;
 step4.intrinsic = ['./neededData/',intrinsics_name];
@@ -124,7 +120,8 @@ step4.path = pathAone;
 info = calcExtrinsics(step4);
 save([savepath 'info.mat'], 'info');
 disp('step4 finished');
-%% 这一步要将区域提取出来
+%% 这一步要将ROI区域提取出来
+% This step extracts the ROI region
 disp('----------step5 start----------')
 
 step5.savepath = savepath;
@@ -142,7 +139,7 @@ step5.gaussian_kernel = gaussian_kernel;
 [pixelImg] = getPixelImage(step5, info);
 disp('step5 finish!');
 
-%%
+%% 旋转，可选 Rotation, optional
 disp('----------step6 start----------');
 
 step6.pixelpath = [savepath 'pixelImgo.mat'];
